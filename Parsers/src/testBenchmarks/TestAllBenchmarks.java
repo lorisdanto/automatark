5055ae99922bfbf4417c97dc6c190b17236289f1
FangWang <terrywang2720@gmail.com>
2016-10-21 22:11:43 -0500

    deleted duplicate in NFA and tree-automata folder

--- Parsers/src/testBenchmarks/TestAllBenchmarks.java
+++ Parsers/src/testBenchmarks/TestAllBenchmarks.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.*;
 
 import java.io.FileReader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -24,7 +25,7 @@ import TreeAutomataParser.TreeNode;
 import TreeAutomataParser.TreeParserProvider;
 
 public class TestAllBenchmarks {
-
+	@Test
 	public void testLTL() {
 		//redirect and gather error message to String for easier debugging
 		ByteArrayOutputStream errMsgs = new ByteArrayOutputStream();
@@ -74,7 +75,7 @@ public class TestAllBenchmarks {
 		assertTrue(noFail);
 	}
 
-	
+	@Test
 	public void testRegex() {
 		ByteArrayOutputStream errMsgs = new ByteArrayOutputStream();
 		System.setErr(new PrintStream(errMsgs));
@@ -124,7 +125,7 @@ public class TestAllBenchmarks {
 		assertTrue(noFail);
 	}
 
-	
+	@Test
 	public void testNFA() {
 		ByteArrayOutputStream errMsgs = new ByteArrayOutputStream();
 		System.setErr(new PrintStream(errMsgs));
@@ -148,6 +149,7 @@ public class TestAllBenchmarks {
 						}
 						System.out.println(filePath);
 					} catch (Exception e1) {
+						System.err.println(filePath);
 						e1.printStackTrace();
 					}
 				}
@@ -164,6 +166,13 @@ public class TestAllBenchmarks {
 			System.err.println("Found duplicate formulas");
 			for (String s : dupFilePath) {
 				System.err.println("Duplicate found at: "+ s);
+				Path p = Paths.get(s);
+				try{
+					Files.delete(p);
+					System.out.println("deleted "+ s);
+				}catch(Exception e){
+					System.out.println("Error deleting " +s +": "+ e);
+				}
 			}
 		}
 
@@ -178,8 +187,8 @@ public class TestAllBenchmarks {
 	
 	@Test
 	public void testTreeAutomata() {
-		//ByteArrayOutputStream errMsgs = new ByteArrayOutputStream();
-		//System.setErr(new PrintStream(errMsgs));
+		ByteArrayOutputStream errMsgs = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(errMsgs));
 
 		boolean noFail = true;
 		HashSet<String> set = new HashSet<>();
@@ -200,6 +209,7 @@ public class TestAllBenchmarks {
 						}
 						System.out.println(filePath);
 					} catch (Exception e1) {
+						System.err.println(filePath);
 						e1.printStackTrace();
 					}
 				}
@@ -217,63 +227,13 @@ public class TestAllBenchmarks {
 			System.err.println("Found duplicate formulas");
 			for (String s : dupFilePath) {
 				System.err.println("Duplicate found at: "+ s);
-			}
-		}
-
-		//String errors = errMsgs.toString();
-//		if(errors.length()>0){
-//			System.err.println("Found errors!");
-//			System.err.println(errors);
-//		}
-		
-		assertTrue(noFail);
-	}
-	
-	
-	/*
-	 * This seems does not follow Timbuk format
-	 * 
-	 * */
-	public void testSymbolicAutomata() {
-		ByteArrayOutputStream errMsgs = new ByteArrayOutputStream();
-		System.setErr(new PrintStream(errMsgs));
-
-		boolean noFail = true;
-		HashSet<String> set = new HashSet<>();
-		ArrayList<String> dupFilePath = new ArrayList<>();
-		try {
-			Files.walk(Paths.get("../symbolic-tree-automata/")).forEach(filePath -> {
-				if (Files.isRegularFile(filePath) && (filePath.toString().endsWith(".timbuk"))) {
-					try {
-						FileReader file = new FileReader(filePath.toFile());
-						System.out.println(filePath);
-						TreeNode node = TreeParserProvider.parse(file);
-						System.out.println(filePath + "parsed correctly");
-						String cur = node.toString();
-						if (set.contains(cur)) {
-							dupFilePath.add(filePath.toString());
-						} else {
-							set.add(cur);
-						}
-					
-					} catch (Exception e1) {
-						e1.printStackTrace();
-					}
+				Path p = Paths.get(s);
+				try{
+					Files.delete(p);
+					System.out.println("deleted "+ s);
+				}catch(Exception e){
+					System.out.println("Error deleting " +s +": "+ e);
 				}
-			});
-		} catch (Exception e) {
-			e.printStackTrace();
-			
-			noFail = false;
-		}finally {
-			System.setErr(new PrintStream(new FileOutputStream(FileDescriptor.err)));
-		}
-		if (dupFilePath.isEmpty()) {
-			System.out.println("No duplicate formulas");
-		} else {
-			System.err.println("Found duplicate formulas");
-			for (String s : dupFilePath) {
-				System.err.println("Duplicate found at: "+ s);
 			}
 		}
 
@@ -285,5 +245,6 @@ public class TestAllBenchmarks {
 		
 		assertTrue(noFail);
 	}
+	
 
 }
