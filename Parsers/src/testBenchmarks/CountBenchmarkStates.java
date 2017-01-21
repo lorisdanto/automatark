7a349fed507a8d179c5787df09c53eaa8e6b0e75
Wang Fang <terrywang2720@gmail.com>
2017-01-20 22:22:36 -0600

    small change to state calculation

--- Parsers/src/testBenchmarks/CountBenchmarkStates.java
+++ Parsers/src/testBenchmarks/CountBenchmarkStates.java
@@ -1,13 +1,8 @@
 package testBenchmarks;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.FileDescriptor;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
 import java.math.BigInteger;
 import java.util.*;
-import static org.junit.Assert.*;
 
 import java.io.FileReader;
 import java.nio.file.Files;
@@ -15,36 +10,40 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import org.junit.Test;
+
 
 import LTLparser.LTLNode;
 import LTLparser.LTLParserProvider;
 
-import RegexParser.RegexParserProvider;
-import RegexParser.RegexNode;
-import TreeAutomataParser.TreeNode;
-import TreeAutomataParser.TreeParserProvider;
-
 
 public class CountBenchmarkStates {
-	BigInteger LTLStateSum = BigInteger.valueOf(0);
-	long LTLNodeCount = (long)0;
-	BigInteger LTLAverage = BigInteger.valueOf(0);
+	static BigInteger LTLStateSum = BigInteger.valueOf(0);
+	static long LTLNodeCount = (long)0;
+	static BigInteger LTLAverage = BigInteger.valueOf(0);
 	
-	BigInteger RegexSizeSum = BigInteger.valueOf(0);
-	long RegexCount = (long)0;
-	BigInteger RegexAverage = BigInteger.valueOf(0);
+	static BigInteger RegexSizeSum = BigInteger.valueOf(0);
+	static long RegexCount = (long)0;
+	static BigInteger RegexAverage = BigInteger.valueOf(0);
 	
-	BigInteger NFATransitionSum = BigInteger.valueOf(0);
-	long NFACount = (long)0;
-	BigInteger NFAAverage = BigInteger.valueOf(0);
+	static BigInteger NFATransitionSum = BigInteger.valueOf(0);
+	static long NFACount = (long)0;
+	static BigInteger NFAAverage = BigInteger.valueOf(0);
 	
-	BigInteger TreeAutTransitionSum = BigInteger.valueOf(0);
-	long TreeAutCount = (long)0;
-	BigInteger TreeAutAverage = BigInteger.valueOf(0);
+	static BigInteger TreeAutTransitionSum = BigInteger.valueOf(0);
+	static long TreeAutCount = (long)0;
+	static BigInteger TreeAutAverage = BigInteger.valueOf(0);
 	
-	@Test
-	public void countLTL() {		
+	public static void main(String args[]){
+		countLTL();
+		countRegex();
+		countNFA();
+		countTreeAutomata();
+		printStatesInfo();
+		
+	}
+	
+	
+	public static void countLTL() {		
 		try {
 		Files.walk(Paths.get("../LTL/")).forEach(filePath -> {
 			if (Files.isRegularFile(filePath) && (filePath.toString().endsWith(".ltl"))) {
@@ -67,13 +66,13 @@ public class CountBenchmarkStates {
 		}
 		
 		LTLAverage = LTLStateSum.divide(BigInteger.valueOf(LTLNodeCount));
-		System.out.println("number of LTLs: "+ LTLNodeCount);
-		System.out.println("total number of LTL states: "+ LTLStateSum);
-		System.out.println("average state of LTLs: "+ LTLAverage);
+//		System.out.println("number of LTLs: "+ LTLNodeCount);
+//		System.out.println("total number of LTL states: "+ LTLStateSum);
+//		System.out.println("average state of LTLs: "+ LTLAverage);
 }
 
-	@Test
-	public void countRegex() {
+	
+	public static void countRegex() {
 		try {
 			Files.walk(Paths.get("../regex/")).forEach(filePath -> {
 				if (Files.isRegularFile(filePath) && (filePath.toString().endsWith(".re"))) {
@@ -95,13 +94,13 @@ public class CountBenchmarkStates {
 		}
 			
 		RegexAverage = RegexSizeSum.divide(BigInteger.valueOf(RegexCount));
-		System.out.println("number of Regex: "+ RegexCount);
-		System.out.println("total Regex size: "+ RegexSizeSum);
-		System.out.println("average size of Regex: "+ RegexAverage);
+//		System.out.println("number of Regex: "+ RegexCount);
+//		System.out.println("total Regex size: "+ RegexSizeSum);
+//		System.out.println("average size of Regex: "+ RegexAverage);
 	}
 
-	@Test
-	public void countNFA() {
+	
+	public static void countNFA() {
 		
 		try {
 			Files.walk(Paths.get("../NFA/")).forEach(filePath -> {
@@ -127,13 +126,13 @@ public class CountBenchmarkStates {
 		}
 			
 		NFAAverage = NFATransitionSum.divide(BigInteger.valueOf(NFACount));
-		System.out.println("number of NFAs: "+ NFACount);
-		System.out.println("total NFA transitions: "+ NFATransitionSum);
-		System.out.println("average transitions of NFA: "+ NFAAverage);
+//		System.out.println("number of NFAs: "+ NFACount);
+//		System.out.println("total NFA transitions: "+ NFATransitionSum);
+//		System.out.println("average transitions of NFA: "+ NFAAverage);
 	}
 	
-	@Test
-	public void testTreeAutomata() {
+	
+	public static void countTreeAutomata() {
 		
 		try {
 			Files.walk(Paths.get("../tree-automata/")).forEach(filePath -> {
@@ -159,6 +158,24 @@ public class CountBenchmarkStates {
 		}
 			
 		TreeAutAverage = TreeAutTransitionSum.divide(BigInteger.valueOf(TreeAutCount));
+//		System.out.println("number of TreeAutomata: "+ TreeAutCount);
+//		System.out.println("total TreeAutomata transitions: "+ TreeAutTransitionSum);
+//		System.out.println("average transitions of TreeAutomata: "+ TreeAutAverage);
+	}
+	
+	public static void printStatesInfo(){
+		System.out.println("number of LTLs: "+ LTLNodeCount);
+		System.out.println("total number of LTL states: "+ LTLStateSum);
+		System.out.println("average state of LTLs: "+ LTLAverage);
+		System.out.println();
+		System.out.println("number of Regex: "+ RegexCount);
+		System.out.println("total Regex size: "+ RegexSizeSum);
+		System.out.println("average size of Regex: "+ RegexAverage);
+		System.out.println();
+		System.out.println("number of NFAs: "+ NFACount);
+		System.out.println("total NFA transitions: "+ NFATransitionSum);
+		System.out.println("average transitions of NFA: "+ NFAAverage);
+		System.out.println();
 		System.out.println("number of TreeAutomata: "+ TreeAutCount);
 		System.out.println("total TreeAutomata transitions: "+ TreeAutTransitionSum);
 		System.out.println("average transitions of TreeAutomata: "+ TreeAutAverage);
